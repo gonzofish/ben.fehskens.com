a87be4bd72426e001fc06dd16d505ac91877432e
Matt Fehskens <matt.fehskens@gmail.com>
2024-05-01 22:02:47 -0400

    Change how controls display

--- scripts/fesh.js
+++ scripts/fesh.js
@@ -13,11 +13,13 @@
     const target = document.querySelector(selector);
     const [firstFile, ...otherFiles] = files;
     const firstImage = createImage(firstFile);
-    const imageContainer = document.createElement('span');
+    const imageContainer = document.createElement('div');
+    const controlsContainer = document.createElement('div');
     const nextControl = document.createElement('button');
     const prevControl = document.createElement('button');
 
     target.classList.add('fesh-gallery');
+    controlsContainer.classList.add('controls');
 
     firstImage.classList.add('active');
     imageContainer.appendChild(firstImage);
@@ -25,21 +27,9 @@
     for (const file of otherFiles) {
       imageContainer.appendChild(createImage(file));
     }
-
-    nextControl.classList.add('control', 'next');
-    nextControl.appendChild(document.createTextNode('➡️'));
-    nextControl.addEventListener('click', () => {
-      const currentActive = getActiveImage(target);
-      const nextActive = imageContainer.lastChild === currentActive ? imageContainer.firstChild : currentActive.nextSibling;
-
-      currentActive.classList.remove('active');
-      nextActive.classList.add('active');
-    });
-    target.appendChild(nextControl);
-
     target.appendChild(imageContainer);
 
-    prevControl.classList.add('control', 'prev');
+    prevControl.classList.add('prev');
     prevControl.appendChild(document.createTextNode('⬅️'));
     prevControl.addEventListener('click', () => {
       const currentActive = getActiveImage(target);
@@ -48,7 +38,20 @@
       currentActive.classList.remove('active');
       nextActive.classList.add('active');
     });
-    target.appendChild(prevControl);
+    controlsContainer.appendChild(prevControl);
+
+    nextControl.classList.add('next');
+    nextControl.appendChild(document.createTextNode('➡️'));
+    nextControl.addEventListener('click', () => {
+      const currentActive = getActiveImage(target);
+      const nextActive = imageContainer.lastChild === currentActive ? imageContainer.firstChild : currentActive.nextSibling;
+
+      currentActive.classList.remove('active');
+      nextActive.classList.add('active');
+    });
+    controlsContainer.appendChild(nextControl);
+
+    target.appendChild(controlsContainer);
   };
 
   window.fesh = {
